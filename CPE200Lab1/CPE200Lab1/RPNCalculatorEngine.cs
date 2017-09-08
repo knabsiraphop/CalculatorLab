8b88af39e86a5f4273aa8acca0368eb696d24654
Siraphop <Mario130@hotmail.com>
2017-09-08 16:54:03 +0700

    WP stackkkkk

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,50 @@ namespace CPE200Lab1
 {
     class RPNCalculatorEngine : CalculatorEngine
     {
+        private bool isOperator(string str)
+        {
+            switch (str)
+            {
+                case "+":
+                case "-":
+                case "X":
+                case "÷":
+                    return true;
+            }
+            return false;
+        }
 
+        public string Process(string str)
+        {
+            string result,first,second,operate;
+            Stack<string> rpnStack = new Stack<string>();
+            string[] parts = str.Split(' ');
+            for(int i = 0; i < parts.Length; i++)
+            {
+                if (isOperator(parts[i]) == true)
+                {
+                    second = rpnStack.Pop();
+                    first = rpnStack.Pop();
+
+                  
+                }
+                else
+                {
+                    rpnStack.Push(parts[i]);
+                    
+                }
+            }
+            result = calculate(,first,second)
+
+            /*spilt str to parts
+            loop each part
+              if part is number
+                   push to stack
+               if part is operate
+                pop two times = > second ,first operand
+                calculate (operator,first ,second0 => result
+                push result to stack
+                return result;*/
+        }
     }
 }
